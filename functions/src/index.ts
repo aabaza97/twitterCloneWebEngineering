94f1d5b4ebf48ca633197374fa9bc3b7e195f417
Ahmed Abaza <aabaza32@hotmail.com>
2021-12-15 01:00:49 +0200

    commit on master

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,16 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import {composeTweet, getAllTweets} from "./Controller/tweetController";
+import * as cors from "cors";
 
 const App = express();
+const corsHandler = cors({origin: true});
+
 
 App.get("/", (req, res) => res.status(200).send("Hello there...."));
 App.post("/tweets", composeTweet);
 App.get("/tweets", getAllTweets);
+App.use(corsHandler);
 
 exports.App = functions.https.onRequest(App);
 
